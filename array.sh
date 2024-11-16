34f6e733d73b7e8855f691ea6c7f52b908932902
Partha0831 <partha0831@gmail.com>
2024-11-17 01:31:16 +0530

    arrays

---
+++ array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("apple" "kiwi" "banana")
+
+echo "first fruit is ${FRUITS[01]}"
+echo "second fruit is ${FRUITS[2]}"
+echo "third fruit is ${FRUITS[3]}"
+
+
+echo "all fruit are ${FRUITS[@]}"
