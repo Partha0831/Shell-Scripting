c4960271e0af55452757243d5c67310db7d84c0f
Partha0831 <partha0831@gmail.com>
2024-11-17 01:25:02 +0530

    read updated

--- read.sh
+++ read.sh
@@ -4,6 +4,10 @@ echo "Please enter your user name?"
 
 read USERNAME
 
+echo "entered user name is $USERNAME"
+
 echo "Please enter your Password?"
 
 read -s PASSWORD
+
+echo "entered user name is $PASSWORD"
