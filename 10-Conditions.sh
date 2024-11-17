2ecec3b94dd5900ee7367efb333716c269d38c41
Partha0831 <partha0831@gmail.com>
2024-11-17 13:47:07 +0530

    conditions

--- 10-Conditions.sh
+++ 10-Conditions.sh
@@ -3,9 +3,9 @@
 Number1=$1
 Number2=$2
 
-if($Number1>$Number2){
+if [ $Number1 -gt $Number2 ]
+then
     echo "the $Number1 is greater than $Number2"
-  }
-else{
+else
  echo "the $Number2 is greater than $Number1"
-}
+fi
