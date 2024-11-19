01a382c954ae0365e0d4a522e261a3414b4695f1
Partha0831 <partha0831@gmail.com>
2024-11-19 22:09:43 +0530

    logs

--- Logs.sh
+++ Logs.sh
@@ -2,6 +2,12 @@
 
 USERID=$(id -u)
 
+LOG_Folder="/var/log/Shell-Logs"
+Log_File=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+File_Name="$LOG_Folder-$Log_File-$TIMESTAMP"
+mkdir -p $File_Name
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -10,7 +16,7 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$RPlease run Script with Root Credential $N "
+        echo -e "$RPlease run Script with Root Credential $N " &>>$File_Name
         exit 1
     fi
 }
@@ -18,25 +24,33 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$package...$R installation failed $N "
+        echo -e "$package...$R installation failed $N " &>>$File_Name
     else
-        echo -e "$package...$G installation Success $N "
+        echo -e "$package...$G installation Success $N " &>>$File_Name
     fi
+}
+USAGE(){
+    echo -e "$R Usage..Please pass atleast on argument $N"
+    exit 1
 }
 
 
  CHECK_ROOT
+ if [ $# -eq o ]
+ then
+ USAGE
+ fi
 
 for package in $@ 
 do
 dnf list installed $package
 if [ $? -ne 0 ]
 then
-echo "Please install $package"
+echo "Please install $package" &>>File_Name
 dnf install $package -y
 VALIDATE
 else
-echo -e "$package...$G is alteady Installed $N"
+echo -e "$package...$G is alteady Installed $N" &>>$File_Name
 fi
 done
 
