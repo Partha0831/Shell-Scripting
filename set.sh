eee806d0edbc44ae23fdc4fcedcc63a966076c76
Partha0831 <partha0831@gmail.com>
2024-11-24 18:18:21 +0530

    set

--- set.sh
+++ set.sh
@@ -2,6 +2,12 @@
 
 set -e # -e for exit if error accures -ex from debug(gets more info)
 
+failure(){
+
+    echo 'falied at $1 : $2'
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"'
 echo "Hello World!"
 echooo "Hello World!"
 echo "Hello World!"
