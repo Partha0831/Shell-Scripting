8b118ed8c929c5db23ea92fa450971211aae2603
Partha0831 <partha0831@gmail.com>
2024-11-25 23:32:32 +0530

    disk usage

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-#1/bin/bash
+#!/bin/bash
 
 R="\e[31m"
 G="\e[32m"
@@ -7,7 +7,7 @@ N="\e[0m"
 
 Source_Dir=$1
 Tar_Dir=$2
-Days=${3:-14} #if $3 is empty, default is 14 days.
+Days=${3:-14} # If $3 is empty, default is 14 days.
 TimseStamp=$(date +%Y-%m-%d-%H-%M-%S)
 Zip_File="$Tar_Dir/app-log-$TimseStamp.zip"
 
@@ -15,43 +15,55 @@ USAGE(){
     echo -e "$R Usage :: $N sh backup.sh <source> <Dest> <days(optional)>"
 }
 
+# Check if at least two arguments are passed
 if [ $# -lt 2 ]
 then
-USAGE
-exit 1
+    USAGE
+    exit 1
 fi
 
-if [ ! -d $Source_Dir ]
+# Check if the source directory exists
+if [ ! -d "$Source_Dir" ]
 then
-echo " $Source_Dir does not exists"
-exit 1
+    echo "$Source_Dir does not exist"
+    exit 1
 fi
 
-if [ ! -d $Tar_Dir ]
+# Check if the target directory exists
+if [ ! -d "$Tar_Dir" ]
 then
-echo " $Tar_Dir does not exists"
-exit 1
+    echo "$Tar_Dir does not exist"
+    exit 1
 fi
 
-Files=$(find $Source_Dir -name "*.log" -mtime +$Days)
+# Find log files older than the specified number of days
+Files=$(find "$Source_Dir" -name "*.log" -mtime +$Days)
+
 echo "$Files"
 
-if [ ! -z $Files ]
+# Check if files are found
+if [ ! -z "$Files" ]
 then
-echo "Files are found"
-find $Source_Dir -name "*.log" -mtime +$Days | zip "$Zip_File" -@
-if [ -f $Zip_File ]
-then
-echo -e "Zipping File is $G Success $N"
-while IFS= read -r file
-do
-echo "deleting $file"
-rm -rf $file
-done <<< $Files
+    echo "Files are found"
+    # Zip the found log files
+    find "$Source_Dir" -name "*.log" -mtime +$Days | zip "$Zip_File" -@
+    
+    # Check if the zip was successful
+    if [ -f "$Zip_File" ]
+    then
+        echo -e "Zipping File is $G Success $N"
+        
+        # Delete the files if zipping was successful
+        while IFS= read -r file
+        do
+            echo "Deleting $file"
+            rm -rf "$file"
+        done <<< "$Files"
+    else
+        echo -e "Zipping files is $R Failed $N"
+    fi
 else
-echo -e "Zipping files is $R Failed $N"
-fi
-else
-echo "Files not are found"
+    echo "No files are found"
 fi
 
+# to run script as a command like cp first we need to give execute permission and cp file into /bin/<name of command>
---
+++ disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1
+
+Disk_uage=$(f -hT | grep xfs)
+Partiton=5 #real projects, it is usually 75
+# history command used find command ran history
+while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+Folder=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+if [ $USAGE -ge $Partiton ]
+then
+echo "disk usage is used more than : $Partiton folder is :$Folder please check "
+fi
+done <<< $Disk_uage
