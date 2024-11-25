2236e673fd1ae1df2ce6af9d126fdbbebd27e3e9
Partha0831 <partha0831@gmail.com>
2024-11-25 23:33:30 +0530

    disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,7 +2,7 @@
 
 #df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1
 
-Disk_uage=$(f -hT | grep xfs)
+Disk_uage=$(df -hT | grep xfs)
 Partiton=5 #real projects, it is usually 75
 # history command used find command ran history
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
