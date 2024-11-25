0f2be97b58054c0348868d229374d44fdca350b7
Partha0831 <partha0831@gmail.com>
2024-11-25 22:52:16 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -9,6 +9,7 @@ Source_Dir=$1
 Tar_Dir=$2
 Days=${3:-14} #if $3 is empty, default is 14 days.
 TimseStamp=$(date +%Y-%m-%d-%H-%M-%S)
+Zip_File="$Tar_Dir/app-log-$TimseStamp.zip"
 
 USAGE(){
     echo -e "$R Usage :: $N sh backup.sh <source> <Dest> <days(optional)>"
@@ -38,6 +39,21 @@ echo "$Files"
 if [ ! -z $Files ]
 then
 echo "Files are found"
+find $Source_Dir -name "*.log" -mtime +$Days | zip "$Zip_File" -@
+if [ -f $Zip_File ]
+then
+echo -e "Zipping File is $G Suces $N"
+while IFS= read -r file
+do
+echo "deleting $file"
+rm -rf $file
+done<<<Files
+else
+echo -e "Zipping files is $R Failed $N"
+fi
+then
+echo "echo zipping the file"
 else
 echo "Files not are found"
 fi
+
