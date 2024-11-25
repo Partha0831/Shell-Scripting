a5e6eaa22769c1ea3bbaa0360a336220495ca365
Partha0831 <partha0831@gmail.com>
2024-11-25 20:30:57 +0530

    crone tab

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -4,7 +4,7 @@
 #find . -name "*.log" to file files
 #find . -name "*.log" -mtime +14 to find backdated files
 
-Source_Dir=/log
+Source_Dir=/home/ec2-user/log
 G="\e[32m"
 R="\e[32m"
 N="\e[0m"
@@ -14,12 +14,13 @@ then
 echo -e "$Source_Dir $G Exists $N"
 else
 echo -e "$Source_Dir $G does not Exists $N"
+exit 1
 fi
 
 Files=$(find . $Source_Dir -name "*.log")
 echo "Files: $Files"
 
-while IFS= read -r File
+while IFS= read -r File #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
 echo "Deleting $File"
 rm -rf $File
